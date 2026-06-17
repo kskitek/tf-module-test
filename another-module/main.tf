25884c7eebd5633e832fb5d9cb628025578c284a
Krzysztof Skitek <krzysztofs@spacelift.io>
2026-06-17 15:52:28 +0200

    feat: Add another module

---
+++ another-module/main.tf
@@ -0,0 +1,3 @@
+resource "terraform_data" "test_resource_1" {
+  input = var.name
+}
---
+++ another-module/output.tf
@@ -0,0 +1,4 @@
+output "name" {
+  value       = terraform_data.test_resource_1.output
+  description = "The stored name."
+}
