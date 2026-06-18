c0b93bc60086269b705f40026c9647198254c472
kskitek <krzysztof.skitek@gmail.com>
2026-06-18 11:00:26 +0200

    feat: Add secret output (#3)

--- another-module/output.tf
+++ another-module/output.tf
@@ -2,3 +2,9 @@ output "name" {
   value       = terraform_data.test_resource_1.output
   description = "The stored name."
 }
+
+output "name_secret" {
+  value       = terraform_data.test_resource_1.output
+  sensitive   = true
+  description = "The secret name."
+}
